2810334a3a020724948044c28b35aaefd2cfe403
Plivak1 <93728206+Plivak1@users.noreply.github.com>
2022-07-25 00:10:31 +0200

    Add files via upload

---
+++ js/task-02.js
@@ -0,0 +1,18 @@
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
+const ingredientsRef = document.querySelector("#ingredients");
+
+const addIngredient = (array) =>
+  array.map((element) => {
+    const itemOfListEl = document.createElement("li");
+    itemOfListEl.textContent = element;
+    return itemOfListEl;
+  });
+
+ingredientsRef.append(...addIngredient(ingredients));
---
+++ js/task-10.js
@@ -0,0 +1,50 @@
+const refs = {
+  controls: document.querySelector("#controls"),
+  input: document.querySelector('[type="number"]'),
+  boxes: document.querySelector("#boxes"),
+  createBtn: document.querySelector("[data-create]"),
+  clearBtn: document.querySelector("[data-destroy]"),
+};
+
+let startingSize = 30;
+let step = 10;
+let startingFontSize = 10;
+let fontSizeStep = 5;
+
+function createBoxes(amount) {
+  amount = refs.input.value;
+
+  const arrayToInjection = [];
+
+  for (let i = 1; i <= amount; i += 1) {
+    const newBox = document.createElement("div");
+    newBox.style.width = `${startingSize + step}px`;
+    newBox.style.height = `${startingSize + step}px`;
+    newBox.style.backgroundColor = `rgb(${Math.floor(
+      Math.random() * 256
+    )},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
+    newBox.style.margin = "5px auto 5px";
+    newBox.style.display = "flex";
+    newBox.style.alignItems = "center";
+    newBox.style.justifyContent = "center";
+    newBox.style.alignItems = "center";
+    newBox.style.fontSize = `${startingFontSize + fontSizeStep}px`;
+    newBox.textContent = `Hello`;
+
+    step += 10;
+    fontSizeStep += 4;
+
+    arrayToInjection.push(newBox);
+  }
+  refs.boxes.append(...arrayToInjection);
+}
+
+function clearBoxes() {
+  refs.boxes.innerHTML = "";
+  refs.input.value = "";
+  step = 10;
+  fontSizeStep = 5;
+}
+
+refs.createBtn.addEventListener("click", createBoxes);
+refs.clearBtn.addEventListener("click", clearBoxes);
